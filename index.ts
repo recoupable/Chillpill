46c4e156f5681a8dfd9b057c79099b95283f1ec3
Sweets Sweetman <sweetmantech@gmail.com>
2024-11-04 23:34:08 -0300

    I sleep for 5 minutes between action loop runs.

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ const init = async () => {
   // Start polling loop
   while (true) {
     await createActionLoop();
+    // Sleep for 5 minutes (300000 milliseconds)
+    await new Promise((resolve) => setTimeout(resolve, 300000));
   }
 };
 
